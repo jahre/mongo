92ef3a192a3683631f656aa12b15e6c39105503c
MINSK\Ivan_Amelyanenka <Ivan_Amelyanenka@epam.com>
2017-02-24 17:56:07 +0300

    sss

--- routes/index.js
+++ routes/index.js
@@ -5,57 +5,39 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
     var db = req.db;
     var collection = db.get('tree');
-
-    var specialContour = 0;
-    function counter(){
-        specialContour = specialContour + 1;
-        return specialContour;
-    }
-    
-    var lastofus = new Promise(function(resolve, reject){
-        var familyArray = [];
-        getChildren = function(id) {
-            return new Promise(function(resolve, reject){
-                var result = collection.find({parent: id});
-                if(id == 'MongoDB' || id == 'dbm' || id == 'Languages'){
-                    reject();
-                }else{
-                    
-                    resolve(result)
+    var i = 0;
+    var familyArray = [];
+    getChildren = function(id) {
+        return new Promise(function(resolve, reject){
+            var result = collection.find({parent: id});
+            if(id == 'MongoDB' || id == 'dbm' || id == 'Languages' || i==4){
+                if(i==4) { 
+                    console.log(familyArray);
+                    res.render('index', { data: familyArray });
                 }
-
-            
+                reject();
+            }else{
                 
-            }).then(function(successMessage){
-                successMessage.forEach(function(child) {
-                    console.log(child);
-                    
-                    getChildren(child._id); 
-                    familyArray.push(child);
-                    return child; 
-            
-                });
-                
-                
-                
-            })
-            .catch(function () {
-                console.log("Promise Rejected");
-            });           
-            
-        };
-        resolve(getChildren('Books'));
-    });
+                resolve(result)
+            }            
+        }).then(function(successMessage){
+            successMessage.forEach(function(child) {
+                console.log(child);
+                getChildren(child._id); 
+                familyArray.push(child);
+                console.log(i);
+                //return child; 
+                i++;
+            });            
+        })
+        .catch(function () {
+            console.log("Promise Rejected");
+        });           
+        
+    };    
 
-    lastofus.then(function(resq){
-        res.send(resq);
-    }).catch(function () {
-                console.log("111Promise Rejected");
-            });
-    
-
-
-    
+    getChildren('Books');
+    //res.render('index', { data: familyArray });
 });
 
 
