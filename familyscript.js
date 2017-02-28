ae4a2474d7ce1358278ea79e8cf60a01a5fa1a38
jahre <jahre@tut.by>
2017-02-28 09:09:11 +0300

    sort

---
+++ familyscript.js
@@ -0,0 +1,18 @@
+db.getCollection('tree').find({})
+db.tree.find({children:[]}).count();
+db.tree.find({})
+db.tree.insert({
+    _id: "test2",
+    parent: "dbm",
+    children: []
+    
+    })
+    
+    
+db.tree.find({parent: 'Programming'}).sort( { position: -1 } )
+db.tree.aggregate(
+   [
+     { $match: { $parent: "Books" } },
+     { $sort : { position : 1 } }
+   ]
+)
--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,9 @@ router.get('/', function(req, res, next) {
     var documentCount = 0;
     var familyArray = [];
     var familyObject = {};
+    //var gg = {children: []};
+    var gg;
+    var masteryourdemons = {children: ['33']};
     getChildren = function(id, count1, sett = true) {
 
         //=======================================================init
@@ -35,21 +38,25 @@ router.get('/', function(req, res, next) {
         
         //=========================================================action
         return new Promise(function(resolve, reject){
-                console.log('=========' + sett);
+                //console.log('=========' + sett);
                 
                 if(documentCount == 0){
                     documentCount = count1;
-                    console.log('+++++++++' + documentCount);
+                    
                 }
-                console.log(i);
+                //console.log(i);
                 //return child; 
                 i++;
                 var result = collection.find({parent: id});
+                //.sort( { position: -1 } );
                 var childrenCheck = result
                 if(!sett || i == documentCount){
                     if(i == documentCount) { 
-                        //console.log(familyArray);
-                        console.log(familyObject);
+                        console.log(familyArray);
+                        //console.log(familyObject);
+                        //console.log('+++++++++');
+                        //console.log(masteryourdemons);
+                        //console.log('+++++++++');
                         res.render('index', { data: familyArray });
                     }
                     reject();
@@ -59,9 +66,49 @@ router.get('/', function(req, res, next) {
                 }     
                   
         }).then(function(successMessage){
-            
-            successMessage.forEach(function(child) {
-                console.log(child);
+            console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
+            console.log(successMessage);
+            //successMessage.forEach(function(child) {
+                for(var v = 0; v < successMessage.length; v++){
+                    var child = successMessage[v];
+                // masteryourdemons.children[child.position] = child;
+                // masteryourdemons = masteryourdemons.children[child.position];
+
+                function drakon(param, pos){
+                    param.children[pos] = {};
+                    return param.children[pos];
+                };
+                if(i == 1){
+                    console.log(i + '===1===');
+                    //console.log(masteryourdemons.children[0]);
+                    //console.log(child);
+                    masteryourdemons.children[0] = child;
+                    console.log(masteryourdemons.children[0]);
+                    //gg = drakon(masteryourdemons.children[0], 0);        
+                    gg = masteryourdemons.children[0];
+                    console.log(gg);
+                }else{
+                    console.log(i + '===2===');
+                    console.log(gg);
+                    console.log(i + '===3===');
+                    //gg.children[child.position - 1] = child;
+                    console.log(gg.children[0]);
+                    gg.children[0] = {};
+                    gg.children[0] = child;
+                    console.log(gg.children[0]);
+                    console.log(i + '===4===');
+                    //console.log(gg.children[child.position - 1]);
+                    console.log(gg.children[0]);
+                    gg = drakon(gg, 0);
+                    console.log(i + '===5===');
+                    console.log(gg);
+                };
+                
+
+                
+
+
+                //console.log(child);
 
                 // function makeJson(depth){
                 //     if(i == 0){
@@ -83,24 +130,24 @@ router.get('/', function(req, res, next) {
                 //     depth.children[k] = getChildren(child._id, child.children.length); 
                 // };
 
-                //familyArray.push(child);
-                familyArray.push('22');
-                function midresult(fam){
-                    if(i == 1){
-                        familyObject = fam;
-                    }
-                    return fam.children
-                };
-                midresult(child).push(getChildren(child._id, child.children.length))
+                familyArray.push(child);
+                // familyArray.push('22');
+                // function midresult(fam){
+                //     if(i == 1){
+                //         familyObject = fam;
+                //     }
+                //     return fam.children
+                // };
+                // midresult(child).push(getChildren(child._id, child.children.length))
                 //familyObject.children.push(child);
                  
                 //if(child.children.length){
                     
-                    //getChildren(child._id, child.children.length); 
-               // }
+                    getChildren(child._id, child.children.length); 
+                //}
                 
                 
-            });            
+            };            
         })
         .catch(function () {
             console.log("Promise Rejected");
