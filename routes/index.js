22c9c078d341428e0a789c59948df618190733ba
MINSK\Ivan_Amelyanenka <Ivan_Amelyanenka@epam.com>
2017-02-28 15:49:41 +0300

    milestone

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,8 @@ router.get('/', function(req, res, next) {
     var familyObject = {};
     //var gg = {children: []};
     var gg;
-    var masteryourdemons = {children: ['33']};
+    //var masteryourdemons = {children: ['33']};
+    var masteryourdemons = {};
     getChildren = function(id, count1, sett = true) {
 
         //=======================================================init
@@ -47,88 +48,78 @@ router.get('/', function(req, res, next) {
                 //console.log(i);
                 //return child; 
                 i++;
-                var result = collection.find({parent: id});
-                //.sort( { position: -1 } );
+                //var result = collection.find({parent: id}).sort({position: -1});
+                var result = collection.find({parent: id}, { sort : { position : 1 } });
                 var childrenCheck = result
                 if(!sett || i == documentCount){
                     if(i == documentCount) { 
-                        console.log(familyArray);
+                        //console.log(familyArray);
+                        console.log('masteryourdemons:');
+                        console.log(masteryourdemons);
                         //console.log(familyObject);
                         //console.log('+++++++++');
                         //console.log(masteryourdemons);
                         //console.log('+++++++++');
-                        res.render('index', { data: familyArray });
+                        //res.render('index', { data: familyArray });
+                        res.send(masteryourdemons);
                     }
                     reject();
                 }else{
-                    
                     resolve(result)
                 }     
                   
         }).then(function(successMessage){
             console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
             console.log(successMessage);
-            //successMessage.forEach(function(child) {
-                for(var v = 0; v < successMessage.length; v++){
-                    var child = successMessage[v];
+            successMessage.forEach(function(child) {
+                // for(var v = 0; v < successMessage.length; v++){
+                //     var child = successMessage[v];
                 // masteryourdemons.children[child.position] = child;
                 // masteryourdemons = masteryourdemons.children[child.position];
 
                 function drakon(param, pos){
-                    param.children[pos] = {};
+                    //param.children[pos] = {};
                     return param.children[pos];
                 };
-                if(i == 1){
-                    console.log(i + '===1===');
-                    //console.log(masteryourdemons.children[0]);
-                    //console.log(child);
-                    masteryourdemons.children[0] = child;
-                    console.log(masteryourdemons.children[0]);
-                    //gg = drakon(masteryourdemons.children[0], 0);        
-                    gg = masteryourdemons.children[0];
-                    console.log(gg);
-                }else{
-                    console.log(i + '===2===');
-                    console.log(gg);
-                    console.log(i + '===3===');
-                    //gg.children[child.position - 1] = child;
-                    console.log(gg.children[0]);
-                    gg.children[0] = {};
-                    gg.children[0] = child;
-                    console.log(gg.children[0]);
-                    console.log(i + '===4===');
-                    //console.log(gg.children[child.position - 1]);
-                    console.log(gg.children[0]);
-                    gg = drakon(gg, 0);
-                    console.log(i + '===5===');
-                    console.log(gg);
+
+                var b;
+                var takeNumber = 0;
+                function drakonv2(obj, name) {
+                    console.log('=take: ' + takeNumber);
+                    takeNumber++;
+                    for (var c = 0; c < obj.children.length; c++) {
+                        if (obj.children[c] == name) {
+                        console.log('yes: ' + obj.children[c]);
+                        console.log(obj);
+                        b = obj;
+                        return
+                        } else if (c == (obj.children.length - 1)) {
+                        console.log('end of list: ');
+                        console.log(obj.children);
+                        var endendOfList = 0;
+                        obj.children.forEach(function(objchild) {
+                            if(typeof objchild == 'object'){
+                            return drakonv2(objchild, name);
+                        }
+                        return          
+                        });
+                            };
+                    };
                 };
+
+                if(i == 1){
+                    console.log('init');
+                    masteryourdemons = child;
+                }else{
+                    console.log('action on: ' + child._id);
+                    console.log(masteryourdemons);
+                    drakonv2(masteryourdemons, child._id);
+                    b.children[child.position] = {};
+                    b.children[child.position] = child;
+                }
                 
 
                 
-
-
-                //console.log(child);
-
-                // function makeJson(depth){
-                //     if(i == 0){
-                //         familyObject = depth;
-                //     }
-                //     depth.id = _id;
-                //     depth.parent = _parent;
-                //     depth.children = [];
-                //     for(let k = 0; k < depth.children.length; k++){
-                //         depth.children[k] = getChildren(depth.children[k], depth.children.length);
-                //     };
-
-                //     return depth;
-                // }
-
-                // makeJson(child);
-
-                // for(let k = 0; k < deti.length; k++){
-                //     depth.children[k] = getChildren(child._id, child.children.length); 
-                // };
 
                 familyArray.push(child);
                 // familyArray.push('22');
@@ -145,9 +136,11 @@ router.get('/', function(req, res, next) {
                     
                     getChildren(child._id, child.children.length); 
                 //}
+
                 
                 
-            };            
+                
+            });            
         })
         .catch(function () {
             console.log("Promise Rejected");
