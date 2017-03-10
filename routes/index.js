4df07a6943da9fd1e981371cf3b01f1b131a73b3
MINSK\Ivan_Amelyanenka <Ivan_Amelyanenka@epam.com>
2017-03-10 15:09:10 +0300

    clean-up for grow

--- routes/index.js
+++ routes/index.js
@@ -4,155 +4,113 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     var db = req.db;
-    var collection = db.get('tree');
+    var collection = db.get('tree');//get family tree collection
     var i = 0;
     var documentCount = 0;
-    var familyArray = [];
-    var familyObject = {};
-    //var gg = {children: []};
-    var gg;
-    //var masteryourdemons = {children: ['33']};
-    var masteryourdemons = {};
-    getChildren = function(id, count1, sett = true) {
+    var familyTree = {};
 
-        //=======================================================init
-         if(count1 == undefined){
-              return new Promise(function(resolve, reject){
-                //initialInfo = collection.find({children:[]}).count();
-                initialInfo = collection.count();
-                //initialInfo = 6;
-                if(initialInfo){
-                    resolve(initialInfo);
-                }else{
-                    reject();
-                };
-                
-              }).then(function(info){
-                getChildren(id, info); 
-           
-        })
-        .catch(function () {
-            console.log("Promise Rejected");
-        });
-                
-         };
-        
-        //=========================================================action
-        return new Promise(function(resolve, reject){
-                //console.log('=========' + sett);
-                
-                if(documentCount == 0){
-                    documentCount = count1;
-                    
-                }
-                //console.log(i);
-                //return child; 
-                i++;
-                //var result = collection.find({parent: id}).sort({position: -1});
-                var result = collection.find({parent: id}, { sort : { position : 1 } });
-                var childrenCheck = result
-                if(!sett || i == documentCount){
-                    if(i == documentCount) { 
-                        //console.log(familyArray);
-                        console.log('masteryourdemons:');
-                        console.log(masteryourdemons);
-                        //console.log(familyObject);
-                        //console.log('+++++++++');
-                        //console.log(masteryourdemons);
-                        //console.log('+++++++++');
-                        //res.render('index', { data: familyArray });
-                        //res.render('index', { data: masteryourdemons });
-                        res.send(masteryourdemons);
-                    }
-                    reject();
-                }else{
-                    resolve(result)
-                }     
-                  
-        }).then(function(successMessage){
-            console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!');
-            console.log(successMessage);
-            var positionC = 0;
-            successMessage.forEach(function(child) {
-                // for(var v = 0; v < successMessage.length; v++){
-                //     var child = successMessage[v];
-                // masteryourdemons.children[child.position] = child;
-                // masteryourdemons = masteryourdemons.children[child.position];
+    getChildren = function (id, collectionLength, settings = true) {
+	//initial request - request to know number of documents
+	if (collectionLength == undefined) {
+		return new Promise(function (resolve, reject) {
+			initialInfo = collection.count();
+			if (initialInfo) {
+				resolve(initialInfo);
+			} else {
+				reject();
+			};
+		})
+			.then(function (info) {
+                //if all good, call funtcion with documents number, and the same id
+				getChildren(id, info);
+			})
+			.catch(function () {
+				console.log("Promise Rejected");
+			});
+	};
 
-                function drakon(param, pos){
-                    //param.children[pos] = {};
-                    return param.children[pos];
-                };
+	//requests to retrieve documents data
+	return new Promise(function (resolve, reject) {
+		if (documentCount == 0) {
+			documentCount = collectionLength;
+		}
+		i++;
 
-                var b;
-                var takeNumber = 0;
-                function drakonv2(obj, name) {
-                    console.log('=take: ' + takeNumber);
-                    takeNumber++;
-                    for (var c = 0; c < obj.children.length; c++) {
-                        if (obj.children[c] == name) {
-                        console.log('yes: ' + obj.children[c]);
-                        console.log(obj);
-                        b = obj;
-                        return
-                        } else if (c == (obj.children.length - 1)) {
-                        console.log('end of list: ');
-                        console.log(obj.children);
-                        var endendOfList = 0;
-                        obj.children.forEach(function(objchild) {
-                            if(typeof objchild == 'object'){
-                            return drakonv2(objchild, name);
-                        }
-                        return          
-                        });
-                            };
-                    };
-                };
+        //sort the documents
+		var result = collection.find({parent: id}, {sort: {position: 1}	});
 
-                if(i == 1){
-                    console.log('init');
-                    masteryourdemons = child;
-                }else{
-                    console.log('action on: ' + child._id);
-                    console.log(masteryourdemons);
-                    drakonv2(masteryourdemons, child._id);
-                    b.children[positionC] = {};
-                    b.children[positionC] = child;
-                }
-                
+		var childrenCheck = result
+		if (!settings || i == documentCount) {
+			if (i == documentCount) {
+				console.log('familyTree:');
+				console.log(familyTree);
+				res.send(familyTree);
+			}
+			reject();
+		} else {
+			resolve(result)
+		}
+	})
+		.then(function (queryDataArray) {
+			var childNumber = 0;
+			queryDataArray.forEach(function (child) {
+				var familyTreeBuffer;
 
                 
+				var iterationNumber = 0;//only for display iteration number in console
 
-                familyArray.push(child);
-                // familyArray.push('22');
-                // function midresult(fam){
-                //     if(i == 1){
-                //         familyObject = fam;
-                //     }
-                //     return fam.children
-                // };
-                // midresult(child).push(getChildren(child._id, child.children.length))
-                //familyObject.children.push(child);
-                 
-                //if(child.children.length){
-                    
-                    getChildren(child._id, child.children.length); 
-                    positionC++
-                //}
+				function retrieveAncestors(obj, name) {
+                    //try to find current Instance name in the list of children of familyTree
+					console.log('==Iteration: ' + iterationNumber);
+					iterationNumber++;
+
+					for (var c = 0; c < obj.children.length; c++) {
+						if (obj.children[c] == name) {
+                            //when name is found, buffer is set to needed depth 
+
+							console.log('Name has been found: ' + obj.children[c]);
+							console.log(obj);
+							familyTreeBuffer = obj;
+							return
+						} else if (c == (obj.children.length - 1)) {
+                            //if there is no children, let's go deper in each nested object, maybe they are there
+
+							console.log('End of list: ');//end of Names list in current object
+							console.log(obj.children);
+							var endendOfList = 0;
+
+							obj.children.forEach(function (objchild) {
+									if (typeof objchild == 'object') {//if object, let's search in it
+										return retrieveAncestors(objchild, name);
+									}
+									return
+								});
+						};
+					};
+				};
+
 
                 
-                
-                
-            });            
-        })
-        .catch(function () {
-            console.log("Promise Rejected");
-        });           
-        
-    };    
+				if (i == 1) {//omit if request is initial
+					familyTree = child;
+				} else {
+					console.log('Finding Ancestors in: ' + child._id);
+					retrieveAncestors(familyTree, child._id);
+					familyTreeBuffer.children[childNumber] = {};//string to object change to set a new value
+					familyTreeBuffer.children[childNumber] = child;//set an object instead of childs name
+				}
 
-    getChildren('Books');
-    //res.render('index', { data: familyArray });
+
+				getChildren(child._id, child.children.length);//next call to retrive other children
+				childNumber++
+			});
+		})
+		.catch(function () {
+			console.log("Promise Rejected");
+		});
+};
+getChildren('Books');
+
 });
 
 
